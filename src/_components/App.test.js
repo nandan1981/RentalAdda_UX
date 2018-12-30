73c00ad3b35dcdb08f722ebca87702db3fe3ccee
Nandan1981 <Nandan1981@gmail.com>
2018-12-30 23:20:30 +0530

    Adding cases

--- src/_components/App.test.js
+++ src/_components/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
+
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() });
 
 
 it('renders without crashing', () => {
@@ -17,3 +21,14 @@ it('renders but crashes', () =>{
     ReactDOM.unmountComponentAtNode(div)
 });
 
+it('renders and contains', () =>{
+ const div = shallow(<App/>);
+ const welcome = 'Learn React . Build RentalAdda';
+ expect(div.contains(welcome)).to.equal(true);
+});
+
+it('renders and does not contain', () =>{
+    const div = shallow(<App/>);
+    const welcome = 'Learn React ..... Build RentalAdda';
+    expect(div.contains(welcome)).to.equal(false);
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './_components/App';
+import App from './_components/App';
 import * as serviceWorker from './serviceWorker';
 
-import Sell from './_services/Sell';
+//import Sell from './_services/Sell';
 
 var message = "Yo yo";
 
-ReactDOM.render(<Sell titleMessage={message}/>, document.getElementById('root'));
-//ReactDOM.render(<App titleMessage={message}/>, document.getElementById('root'));
+//ReactDOM.render(<Sell titleMessage={message}/>, document.getElementById('root'));
+ReactDOM.render(<App titleMessage={message}/>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
