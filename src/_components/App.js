029f14f04d1fdcee712184a84be345fbd27165ed
Nandan1981 <Nandan1981@gmail.com>
2019-02-17 17:28:52 +0530

    Mods to the nav header

--- src/_components/App.js
+++ src/_components/App.js
@@ -16,22 +16,34 @@ class App extends Component {
     this.enableMainPage = this.enableMainPage.bind(this);
      this.enableBuyPage = this.enableBuyPage.bind(this);
      this.enableRentPage = this.enableRentPage.bind(this);
+     this.showSignUpPage = this.showSignUpPage.bind(this);
+     this.showNavHeader = this.showNavHeader.bind(this);
   };
 
   componentDidMount() {
-      fetch('http://localhost:8082/RentalAddaRest/customers.htm')
-          .then(customers => {
-              return customers.json();
-          }).then(data => {
-          let pictures = data.customers.map((pic) => {
-              return (
-                  <div key={pic.customers}>
-                      <img src={pic.picture.medium}/>
-                  </div>
-              )
-          })
-          this.setState({pictures: pictures});
-      })
+      fetch('http://localhost:9202/fetchRentalData/Nandan', {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          mode: "no-" +
+          "cors", // no-cors, cors, *same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, *same-origin, omit
+          headers: {
+              "Content-Type": "application/json",
+              // "Content-Type": "application/x-www-form-urlencoded",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrer: "no-referrer", // no-referrer, *client
+       });//.then(results => {
+      //     return results.json();
+      // }).then(data => {
+      //     let output = data.results.map((names) => {
+      //         return(
+      //             <p>{names.firstName}</p>
+      //         )
+      // })
+
+      //this.setState({pictures: output});
+    //})
   }
 
     enableMainPage(){
@@ -52,6 +64,65 @@ class App extends Component {
             isShowRentPage: false});
     }
 
+    showSignUpPage(){
+      alert('in here');
+        this.setState({isShowMainPage: false,
+            isShowBuyPage: true,
+            isShowRentPage: false});
+    }
+
+    openPopupLogin(){
+        var win = window.open('https://localhost:8048/LoginPopup','signup','height=400,width=400,top=400,left=600,toolbar=no,scrollbars=no,status=no,titlebar=no,menubar=no,location=no');
+        if (window.focus) {
+            win.focus()}
+        return false;
+    }
+
+    showNavHeader(){
+        return (  <nav className="navbar navbar-inverse navbar-fixed-top">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <img src={process.env.PUBLIC_URL +'/logo_nodesc.png'} className="logo"/>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li className="active"><a href="#">Home</a></li>
+                    <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown"
+                                                href="#"><span className="caret"></span></a>
+                        <ul className="dropdown-menu">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Why Us?</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Products</a></li>
+                </ul>
+                <form className="navbar-form navbar-left" action="/action_page.php">
+                    <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Search the entire website"
+                               name="search"></input>
+                        <div className="input-group-btn">
+                            <button className="btn btn-default" type="submit">
+                                <i className="glyphicon glyphicon-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <div className="navbar-header"
+                     className="navbar-header-buy">RENTALADDA
+                    <ul className="nav navbar-nav navbar-right">
+                        <li ><a href="#" onClick={this.showSignUpPage}><span
+                            className="glyphicon"></span> Sign Up</a></li>
+                        <li><a href="#" onClick={this.openPopupLogin}><span
+                            className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        <li><a href="#"><span className="glyphicon glyphicon-user"></span> My
+                            Profile</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>);
+    }
+
     render() {
         var inlineStyle = {display: this.state.isVisible ? "block" : "none" };
         let mainPage = this.state.isShowMainPage;
@@ -60,13 +131,11 @@ class App extends Component {
         let Page = null;
 
         if(mainPage){
-
              Page = (
                 <div className="App">
                     <p> {this.props.isVisible}</p>
                     <header className="App-header">
                         <p>
-
                             <button type="button" id="Buy" className="btn btn-lg" onClick={this.enableBuyPage}>Buy
                             </button>
                             <button type="button" id="Rent" className="btn btn-lg" onClick={this.enableRentPage}>Rent
@@ -89,7 +158,7 @@ class App extends Component {
             );
         }else if(buyPage){
             Page = (
-                <div className="Buy align-middle text-white">
+                <div className="Buy align-middle text-white ">
                     <p> {this.props.isVisible}</p>
                     <header>
                         <form>
@@ -142,6 +211,7 @@ class App extends Component {
         }
 
             return( <div>
+                {this.showNavHeader()}
                 {Page}
             </div> );
 
